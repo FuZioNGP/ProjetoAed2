7e01025f61c86cb1e2d811a079a39e9dfcf25e3d
FuZioNGP <fuzionsamp@hotmail.com>
2021-11-26 13:43:15 -0300

    Criado comandos iniciais

--- Program.cs
+++ Program.cs
@@ -1,33 +1,92 @@
-﻿using Discord.WebSocket;
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
 using System;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Discord.app
 {
-	public class Program
+	class Program
     {
+        
         private DiscordSocketClient _client;
+        private CommandService _commands;
         private Task Log(LogMessage msg)
 		{
 			Console.WriteLine(msg.ToString());
 			return Task.CompletedTask;
 		}
-		public static void Main(string[] args)
-			=> new Program().MainAsync().GetAwaiter().GetResult();
+        public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
 
-		public async Task MainAsync()
+        public async Task MainAsync()
 		{
-            _client = new DiscordSocketClient();
+                _client = new DiscordSocketClient();
+                _commands = new CommandService();
 
-            _client.Log += Log;
+                _client.Log += Log;
 
-            var token = "ODIyOTgzMzU4OTc1NzA1MTE5.YFaM-w.Us8v9__0qjZg4qdw9p2khQ5CASM";
+                var token = "ODIyOTgzMzU4OTc1NzA1MTE5.YFaM-w.Us8v9__0qjZg4qdw9p2khQ5CASM";
+                await Client_Ready();
+                await InstallCommandsAsync();
 
-            await _client.LoginAsync(TokenType.Bot, token);
-            await _client.StartAsync();
+                await _client.LoginAsync(TokenType.Bot, token);
+                await _client.StartAsync();
+               
 
             // Block this task until the program is closed.
             await Task.Delay(-1);
+          
         }
-	}
+        private async Task Client_Ready()
+        {
+            await _client.SetGameAsync("WLS SERVER", type: ActivityType.Playing);
+        }
+        private async Task InstallCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(), services: null);
+        }
+        private async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+            int argPos = 0;
+            if (!(message.HasCharPrefix('!', ref argPos) ||
+                message.HasMentionPrefix(_client.CurrentUser, ref argPos)) ||
+                message.Author.IsBot)
+                return;
+
+            var context = new SocketCommandContext(_client, message);
+
+            await _commands.ExecuteAsync(
+                context: context,
+                argPos: argPos,
+                services: null);
+        }
+
+
+        public async Task CommandExecutedAsync(Optional<CommandInfo> command, ICommandContext context, IResult result)
+        {
+            // if a command isn't found, log that info to console and exit this method
+            if (!command.IsSpecified)
+            {
+                System.Console.WriteLine($"Command failed to execute for [{context.User.Username}] <-> [{result.ErrorReason}]!");
+                return;
+            }
+
+
+            // log success to the console and exit this method
+            if (result.IsSuccess)
+            {
+                System.Console.WriteLine($"Command [{command.Value.Name}] executed for -> [{context.User.Username}]");
+                return;
+            }
+
+
+            // failure scenario, let's let the user know
+            await context.Channel.SendMessageAsync($"Sorry, {context.User.Username}... something went wrong -> [{result}]!");
+        }
+    }
 }
