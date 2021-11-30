d498910b647c572304ed6d9b2242a9208d4dc48e
FuZioNGP <fuzionsamp@hotmail.com>
2021-11-30 14:39:41 -0300

    Criação da regra administrativa.

--- Comandos/AdminCmds.cs
+++ Comandos/AdminCmds.cs
@@ -3,28 +3,47 @@ using System;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-
+using Discord.WebSocket;
+using System.Linq;
 
 namespace Discord.app.AdminCommands
 {
-    [Group("ADMINISTRADOR")]
     public class AdminCommands : ModuleBase
     {
         [Command("teste")]
         public async Task AdminCommand()
         {
+            var User = Context.User as SocketGuildUser;
+            var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == "ADMINISTRADOR");
+
+            if (User.Roles.Contains(role))
+            {
+                var sb = new StringBuilder();
+
+                // get user info from the Context
+
+                // build out the reply
+                sb.AppendLine($"Seu nick é -> " + User);
+                sb.AppendLine("E você é administrador");
+
+                // send simple string reply
+                await ReplyAsync(sb.ToString());
+            }
+            else
+            {
+                var sb = new StringBuilder();
+
+                // get user info from the Context
+
+                // build out the reply
+                sb.AppendLine($"Seu nick é -> " + User);
+                sb.AppendLine("E você não é administrador");
+
+                // send simple string reply
+                await ReplyAsync(sb.ToString());
+            }
             // initialize empty string builder for reply
-            var sb = new StringBuilder();
 
-            // get user info from the Context
-            var user = Context.User;
-
-            // build out the reply
-            sb.AppendLine($"Seu nick é -> " + user);
-            sb.AppendLine("E você é administrador");
-
-            // send simple string reply
-            await ReplyAsync(sb.ToString());
         }
     }
 }
